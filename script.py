be94e09b219725aa2938b24d825a43c5c83b6af3
GabrielV <101893299+Gabriel-Vergely@users.noreply.github.com>
2025-05-20 13:06:57 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,1 @@
-import os
-import subprocess
-
-def eliminar_archivo(nombre_archivo):
-    try:
-        os.remove(nombre_archivo)
-    except FileNotFoundError:
-        print("Archivo no encontrado.")
 
